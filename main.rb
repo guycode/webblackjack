0a0c392009ea4cdc8a9427a68105091dc0c8b576
Rich Lane <therealrichlane@gmail.com>
2014-12-21 23:54:35 -0700

    Display of both dealer and player hands

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 
+set :sessions, true
+
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'AvaCash'
@@ -8,6 +10,7 @@ use Rack::Session::Cookie, :key => 'rack.session',
 
 get '/' do
   if session[:player_name]
+    redirect '/game'
   else
     redirect 'new_player'
   end
@@ -17,6 +20,11 @@ get '/new_player' do
   erb :new_player
 end
 
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
 post 'set_name' do 
   session[:player_name] = params[:player_name]
   redirect '/game'
@@ -24,6 +32,18 @@ end
 
 
 get '/game' do 
+  suits = ['H','D','S','C']
+  values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop  
+
+
   erb :game
 end
 
